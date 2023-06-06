61c71b195901a6b3517d2b31096e9781f27d72a0
hv25-waikato <hv25@students.waikato.ac.nz>
2023-06-06 17:31:00 +1200

    Welcome screen buttons done. Working on functionality using FileChooser

---
+++ src/main/java/ictgradschool/industry/final_project/InventoryItems.java
@@ -0,0 +1,22 @@
+package ictgradschool.industry.final_project;
+
+import javax.swing.table.AbstractTableModel;
+
+public class InventoryItems extends AbstractTableModel {
+    @Override
+    public int getRowCount() {
+        return 0;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return null;
+    }
+
+
+}
--- src/main/java/ictgradschool/industry/final_project/rootManagerFrame.java
+++ src/main/java/ictgradschool/industry/final_project/rootManagerFrame.java
@@ -11,6 +11,10 @@ public class rootManagerFrame extends JPanel implements ActionListener {
     private JButton Open;
     private JButton Create;
 
+    private JButton Close;
+    private JFileChooser fileChooser;
+
+
 
     public rootManagerFrame() {
         JFrame frame = new JFrame("Product Manager");
@@ -22,11 +26,17 @@ public class rootManagerFrame extends JPanel implements ActionListener {
         Open = new JButton("Select existing Filestore");
 //        Open.setPreferredSize(new Dimension(100, 30));
         contentpane.add(Open);
+        Open.addActionListener(this);
 
         Create = new JButton("Create new Filestore");
 //        Create.setPreferredSize(new Dimension(100, 30));
         contentpane.add(Create);
         Create.addActionListener(this);
+
+        Close = new JButton("Close");
+        contentpane.add(Close);
+        Close.addActionListener(this);
+
 
         frame.setContentPane(contentpane);
         frame.setVisible(true);
@@ -40,16 +50,34 @@ public class rootManagerFrame extends JPanel implements ActionListener {
         }
         if (e.getSource()==Open){
             final JFileChooser fc = new JFileChooser();
-            fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+//            fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
             fc.setCurrentDirectory(new File("../src/main/resources/"));
             int returnVal=fc.showDialog(rootManagerFrame.this,"Open");
 
             if(returnVal==JFileChooser.APPROVE_OPTION){
-                File directory=fc.getSelectedFile();
+                File directory=fc.getCurrentDirectory().getAbsoluteFile();
+                File file=fc.getSelectedFile();
+                String fileName=file.getName();
+                String nameExtension=fileName.substring(fileName.lastIndexOf(".")+1);
+                if (nameExtension.equalsIgnoreCase("csv")){
+                    JOptionPane.showMessageDialog(this,"You have selected to open "+fileName);
+
+
+                    System.out.println("FIle valid & opening");
+
+                }else{
+                    System.out.println("File invalid");
+                    JOptionPane.showMessageDialog(this,"File invalid");
+
+                }
+
 
                 System.out.println(directory);
             }
 
+        }
+        if (e.getSource()==Close){
+            System.exit(0);
         }
 
 
